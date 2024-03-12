955180e5bf58e603cc4e25a2fde5e0fa89ee00a1
Ruan Chagas <ruanchagas95@gmail.com>
2024-03-11 22:00:48 -0300

    feat: req16 filter matches

--- app/backend/src/controllers/matchesControllers.ts
+++ app/backend/src/controllers/matchesControllers.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { getAllMatches } from '../services/matchesService';
 
-export const getAllMatchesController = async (_req: Request, res: Response) => {
-  const matches = await getAllMatches();
+export const getAllMatchesController = async (req: Request, res: Response) => {
+  const inProgress = req.query.inProgress === undefined ? null : req.query.inProgress === 'true';
+  const matches = await getAllMatches(inProgress);
   res.status(200).json(matches);
 };
 
--- app/backend/src/services/matchesService.ts
+++ app/backend/src/services/matchesService.ts
@@ -1,9 +1,16 @@
 import MatcheModel from '../database/models/MatchModel';
 
-const createOptions = (association: string) => ({ association, attributes: { exclude: ['id'] } });
+const createOptions = (association: string) => ({
+  association,
+  attributes: { exclude: ['id'] },
+});
 
-export const getAllMatches = () => MatcheModel.findAll(
-  { include: [createOptions('homeTeam'), createOptions('awayTeam')] },
-);
+export const getAllMatches = (inProgress: boolean | null) =>
+  MatcheModel.findAll(
+    {
+      include: [createOptions('homeTeam'), createOptions('awayTeam')],
+      where: inProgress !== null ? { inProgress } : undefined,
+    },
+  );
 
 export default { getAllMatches };
